83e8e1b4f6c3df23aaa5e783c996b10c6073beb6
Lilian PERTHUIS <lilianperthuis@gmail.com>
2025-04-02 17:25:15 +0200

    websocket fastify progress

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -3,9 +3,17 @@ import { LogLevel, LogType } from "././logger/normalization.js";
 import { registerHooks } from "./logger/hook.js";
 import { logToELK } from "./logger/logToElk.js";
 // @ts-ignore
-import { WebSocketServer } from "ws";
+import fastifyWebsocket from "@fastify/websocket";
 import { Player, ServerSidePong } from "./serverSidePong.js";
 const app = fastify();
+// @fastify/websocket
+app.register(fastifyWebsocket);
+app.get("/ws", { websocket: true }, (socket, request) => {
+    socket.on("message", async (message) => {
+        console.log("Message received!");
+    });
+});
+// ELK
 registerHooks(app);
 app.get("/", async (req, reply) => {
     logToELK({
@@ -28,23 +36,28 @@ const start = async () => {
     }
 };
 const game = new ServerSidePong();
-const wss = new WebSocketServer({ port: 4501 });
-wss.on("connection", (ws) => {
-    console.log("Client connected.");
-    const interval = setInterval(() => {
-        if (ws.readyState === ws.OPEN) {
-            const state = {
-                ballX: game.getGame().getBall().getX(),
-                ballY: game.getGame().getBall().getY(),
-            };
-            ws.send(JSON.stringify(state));
-        }
-    }, 1000 / 60);
-    ws.on("close", (code, reason) => {
-        console.log("Client disconnected, code:", code, "reason:", reason.toString());
-        clearInterval(interval);
-    });
-});
+// const wss = new WebSocketServer({ port: 4501 });
+// wss.on("connection", (ws: WebSocket) => {
+//   console.log("Client connected.");
+//   const interval = setInterval(() => {
+//     if (ws.readyState === ws.OPEN) {
+//       const state = {
+//         ballX: game.getGame().getBall().getX(),
+//         ballY: game.getGame().getBall().getY(),
+//       };
+//       ws.send(JSON.stringify(state));
+//     }
+//   }, 1000 / 60);
+//   ws.on("close", (code: number, reason: Buffer) => {
+//     console.log(
+//       "Client disconnected, code:",
+//       code,
+//       "reason:",
+//       reason.toString()
+//     );
+//     clearInterval(interval);
+//   });
+// });
 function startGame() {
     game.launchGame(new Player("Player1"), new Player("Player2"));
     console.log("Game is runing!");
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,24 @@
-import fastify from "fastify";
+import fastify, { FastifyInstance } from "fastify";
 import { LogLevel, LogType } from "././logger/normalization.js";
 import { registerHooks } from "./logger/hook.js";
 import { logToELK } from "./logger/logToElk.js";
 // @ts-ignore
-import { WebSocket, WebSocketServer } from "ws";
+import fastifyWebsocket from "@fastify/websocket";
 import { Player, ServerSidePong } from "./serverSidePong.js";
 
-const app = fastify();
+const app: FastifyInstance = fastify();
+
+// @fastify/websocket
+
+app.register(fastifyWebsocket);
+
+app.get("/ws", { websocket: true }, (socket, request) => {
+  socket.on("message", async (message) => {
+    console.log("Message received!");
+  });
+});
+
+// ELK
 
 registerHooks(app);
 
@@ -33,34 +45,33 @@ const start = async () => {
 
 const game = new ServerSidePong();
 
-const wss = new WebSocketServer({ port: 4501 });
-
 interface GameState {
   ballX: number;
   ballY: number;
 }
+// const wss = new WebSocketServer({ port: 4501 });
 
-wss.on("connection", (ws: WebSocket) => {
-  console.log("Client connected.");
-  const interval = setInterval(() => {
-    if (ws.readyState === ws.OPEN) {
-      const state = {
-        ballX: game.getGame().getBall().getX(),
-        ballY: game.getGame().getBall().getY(),
-      };
-      ws.send(JSON.stringify(state));
-    }
-  }, 1000 / 60);
-  ws.on("close", (code: number, reason: Buffer) => {
-    console.log(
-      "Client disconnected, code:",
-      code,
-      "reason:",
-      reason.toString()
-    );
-    clearInterval(interval);
-  });
-});
+// wss.on("connection", (ws: WebSocket) => {
+//   console.log("Client connected.");
+//   const interval = setInterval(() => {
+//     if (ws.readyState === ws.OPEN) {
+//       const state = {
+//         ballX: game.getGame().getBall().getX(),
+//         ballY: game.getGame().getBall().getY(),
+//       };
+//       ws.send(JSON.stringify(state));
+//     }
+//   }, 1000 / 60);
+//   ws.on("close", (code: number, reason: Buffer) => {
+//     console.log(
+//       "Client disconnected, code:",
+//       code,
+//       "reason:",
+//       reason.toString()
+//     );
+//     clearInterval(interval);
+//   });
+// });
 
 function startGame(): void {
   game.launchGame(new Player("Player1"), new Player("Player2"));
