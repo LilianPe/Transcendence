6edcd444a6b41ca8877cdebbad24a05d56689854
Chaisneau <chaisneau@gmail.com>
2025-04-16 15:06:27 +0200

    import bug fixed

--- frontend/src/frontend.ts
+++ frontend/src/frontend.ts
@@ -1,7 +1,7 @@
 import { handleKeyPress, keys, launchButtonAddEvent, launchTournamentButtonAddEvent, online, onlineButtonAddEvent, registerTournamentButtonAddEvent } from "./events.js";
 import { offMove, renderLocal } from "./Offline/pongAiExecutor.js";
-import { displayLaunchError, displayLine, displayScore, drawLeftPlayer, drawRightPlayer } from "./Online/pongDisplayOnline.js";
-import { currentState, handleWebSocket, targetState } from "./Online/webSocket.js";
+import { displayLaunchError, displayLine, displayScore, displayWinner, drawLeftPlayer, drawRightPlayer } from "./Online/pongDisplayOnline.js";
+import { currentState, handleWebSocket, targetState, tournamentWinner } from "./Online/webSocket.js";
 import { PlayerMoves } from "./Pong/Player.js";
 
 
